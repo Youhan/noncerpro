2f2bbe7d3b46187582a6abcdb1fc987c5ecc90a5
Alireza Jahandideh <ar.jahandideh@gmail.com>
2019-04-28 08:06:59 -0500

    Update DevFee.vue

--- src/components/sections/DevFee.vue
+++ src/components/sections/DevFee.vue
@@ -3,8 +3,8 @@
         <h2 class="is-size-3 ">
             Developer Fee
         </h2>
-        <p>This CUDA miner has a fixed 2% dev fee. That means 2 minutes(plus 20 seconds to compensate for reconnecting and possible invalid shares at the begining) in every 100 minutes, miner will run with the donation wallet address.</p>
-        <p>For the OpenCL/AMD miner, the dev fee is 1%.</p>
+        <p>This miner has a fixed 2% dev fee. That means 2 minutes(plus 20 seconds to compensate for reconnecting and possible invalid shares at the begining) in every 100 minutes, miner will run with the donation wallet address.</p>
+        <p>Dev fee was 1% in AMD miner V1.0.1 and prior versions.</p>
     </div>
 </template>
 
