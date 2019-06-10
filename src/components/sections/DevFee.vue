4a7d6181f4c64dca6201d737712a89a33e13c617
Alireza Jahandideh <ar.jahandideh@gmail.com>
2019-06-10 15:48:36 -0500

    Update DevFee.vue

--- src/components/sections/DevFee.vue
+++ src/components/sections/DevFee.vue
@@ -3,7 +3,7 @@
         <h2 class="is-size-3 ">
             Developer Fee
         </h2>
-        <p>This miner has a fixed 2% dev fee. That means 2 minutes(plus 20 seconds to compensate for reconnecting and possible invalid shares at the begining) in every 100 minutes, miner will run with the donation wallet address.</p>
+        <p>This miner has a fixed 2% dev fee. That means 2 minutes in every 100 minutes, miner will run with the donation wallet address.</p>
         <p>Dev fee was 1% in AMD miner V1.0.1 and prior versions.</p>
     </div>
 </template>
